996c5240e0faafbea0c24693dba0891208b3ed85
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-12 16:41:44 +0800

    Add bin

---
+++ bin/refresh.el
@@ -0,0 +1,14 @@
+;;; refresh.el --- Update archives  -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(load-file "./bin/_prepare.el")
+
+
+()
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
+;;; refresh.el ends here
