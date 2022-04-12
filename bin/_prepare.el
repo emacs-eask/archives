808e5d73c7fd65afdf81ab52fc19db47cca7c811
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-12 17:06:48 +0800

    Fix gen

--- bin/_prepare.el
+++ bin/_prepare.el
@@ -7,20 +7,6 @@
 (require 'json)
 (require 'subr-x)
 
-(defconst archive-contents-string
-  (with-temp-buffer
-    (or (ignore-errors (insert-file-contents "../docs/packages/archive-contents"))
-        (ignore-errors (insert-file-contents "./docs/packages/archive-contents")))  ; CI
-    (buffer-string))
-  "Archive content string.")
-
-(defconst archive-contents
-  (eval (thing-at-point--read-from-whole-string
-         (concat "'" archive-contents-string)))
-  "Turn it into lisp object.")
-
-(pop archive-contents)  ; remove 1
-
 ;;
 ;;; Util
 
