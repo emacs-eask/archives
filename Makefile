b83f33806c06374a42b29efe07159c9ba49d24b9
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-12 17:02:59 +0800

    Done

--- Makefile
+++ Makefile
@@ -11,5 +11,5 @@ generate_badges_system:
 
 refresh:
 	@echo "Refresh archive-contents"
-	@$(EASK) refresh
+	@$(EASK) refresh --allow-error
 	@$(EASK) load "./bin/refresh.el"
--- bin/refresh.el
+++ bin/refresh.el
@@ -2,15 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun shell-execute (cmd &rest args)
-  "Return non-nil if CMD executed succesfully with ARGS."
-  (= 0 (shell-command (concat cmd " "
-                              (mapconcat #'shell-quote-argument args " ")))))
-
-(shell-execute
- "mv"
- (expand-file-name (concat ".eask/" emacs-version "/elpa/archives/"))
- (expand-file-name "./"))
+(let* ((file "archive-contents")
+       (dir (concat ".eask/" emacs-version "/elpa/archives/"))
+       (archives (directory-files dir nil "^\\([^.]\\|\\.[^.]\\|\\.\\..\\)")))
+  (dolist (archive archives)
+    (make-directory (expand-file-name archive) t)
+    (copy-file (expand-file-name (concat archive "/" file) dir)
+               (expand-file-name "./" archive))))
 
 ;; Local Variables:
 ;; coding: utf-8
